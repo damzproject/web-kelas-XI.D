86dba0b7c3fd5cfbda19786009493b9f4553ed58
damzproject <nokosvelzz@gmail.com>
2026-02-28 01:38:18 +0700

    veldam

---
+++ src/components/Mapel/Kamis.jsx
@@ -0,0 +1,51 @@
+const Kamis = () => {
+	return (
+		<>
+			<div
+				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
+				data-aos="fade-up"
+				data-aos-duration="600">
+				<div className="w-[50%] text-base  font-medium">B.indo</div>
+				<div className="flex justify-center items-center text-sm">07.00-08.20</div>
+			</div>
+			<div
+				className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
+				data-aos="fade-up"
+				data-aos-duration="600">
+				<div className="w-[50%] text-base  font-medium">Kimia</div>
+				<div className="flex justify-center items-center text-sm">08.20-10.50</div>
+			</div>
+			<div
+				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
+				data-aos="fade-up"
+				data-aos-duration="700">
+				<div className="w-[50%] text-base  font-medium">Istirahat</div>
+				<div className="flex justify-center items-center text-sm">09.40-10.10</div>
+			</div>
+			<div
+				className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
+				data-aos="fade-up"
+				data-aos-duration="800">
+				<div className="w-[50%] text-base  font-medium">Matematika</div>
+				<div className="flex justify-center items-center text-sm">10.50-12.10</div>
+			</div>
+			<div
+				className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
+				data-aos="fade-up"
+				data-aos-duration="800">
+				<div className="w-[50%] text-base  font-medium">Ishoma</div>
+				<div className="flex justify-center items-center text-sm">12.10-13.00</div>
+			</div>
+
+			<div
+				className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
+				data-aos="fade-up"
+				data-aos-duration="1000">
+				<div className="w-[50%] text-base font-medium">B.Inggris</div>
+				<div className="flex justify-center items-center text-sm">13.00-14.45</div>
+			</div>
+		</>
+	)
+}
+
+export default Kamis
---
+++ src/components/Mapel/Rabu.jsx
@@ -0,0 +1,37 @@
+const Rabu = () => {
+    return (
+        <>
+        	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">Matematika TL</div>
+					<div className="flex justify-center items-center text-sm">06.50-07.30</div>
+				</div>
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">Biologi</div>
+					<div className="flex justify-center items-center text-sm">08.10-10.10</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+					<div className="w-[50%] text-base  font-medium">Istirahat</div>
+					<div className="flex justify-center items-center text-sm">10.10-10.40</div>
+				</div>
+				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+					<div className="w-[50%] text-base  font-medium">Kimia</div>
+					<div className="flex justify-center items-center text-sm">10.40-12.00</div>
+				</div>
+
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
+					<div className="w-[50%] text-base  font-medium">Ishoma</div>
+					<div className="flex justify-center items-center text-sm">12.00-13.00</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
+					<div className="w-[50%] text-base font-medium">B.indo</div>
+					<div className="flex justify-center items-center text-sm">13.00-14.10</div>
+				</div>
+				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
+					<div className="w-[50%] text-base font-medium">Bk</div>
+					<div className="flex justify-center items-center text-sm">14.10-14.45</div>
+				</div>
+        </>
+    )
+} 
+
+export default Rabu;
